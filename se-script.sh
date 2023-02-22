cb7cfeb555b627f98bbe17c68394bc15275e60ac
mohammedelbash <126073011+mohammedelbash@users.noreply.github.com>
2023-02-22 13:39:03 +0200

    Add files via upload

---
+++ se-script.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash 
+count=7
+var1="life is good"
+echo "hello world"
+echo "heloo aws re/start"
+
+echo   "hello  this is your working directory: $(pwd) "
+ sleep 3
+
+echo "good day this is the user home dir :$HOME "
+sleep 3 
+echo "$count"
+echo $var1
+read -p "how old are you "  age
+echo "$age"
+read -p "what is your name" name
+echo "$name"
+read "enter first name and last name"  age
+echo "$age"
+echo "$0"
